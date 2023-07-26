6a3a185f22381196d511c3e2c43192c62f6598b1
Noah McMillan <noahm27@gmail.com>
2023-07-25 18:53:21 -0700

    Function to check if 2 arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actualArray, expectedArray) {
+  // checks that the arrays are the same length
+  if (actualArray.length !== expectedArray.length) {
+    return false;
+  }
+  // check if the value of each element is the same
+  for (let x = 0; x < actualArray.length; x++) {
+    if (actualArray[x] !== expectedArray[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);

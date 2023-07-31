e22265d565766cb94a7fc5e52f5eadf0f897e63f
Noah McMillan <noahm27@gmail.com>
2023-07-31 15:18:18 -0700

    Refactored and added test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,6 @@
-const eqArrays = function(actualArray, expectedArray) {
-  // checks that the arrays are the same length
-  if (actualArray.length !== expectedArray.length) {
-    return false;
-  }
-  // check if the value of each element is the same
-  for (let x = 0; x < actualArray.length; x++) {
-    if (actualArray[x] !== expectedArray[x]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(actual, expected) {
+const assertArraysEqual = (actual, expected) => {
   // checks if the 2 arrays are equal and assigns true or false to isEqual
   let isEqual = eqArrays(actual, expected);
   if (isEqual) {
@@ -22,5 +10,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
+module.exports = assertArraysEqual;

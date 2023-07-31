dc6ae6409aff90f44e38e355563cd10a346a0dc4
Noah McMillan <noahm27@gmail.com>
2023-07-31 15:04:50 -0700

    Refactored the functions and added a test file

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈 Assertion Failed: ${actual} !== ${expected}`);
+const tail = (array) => {
+  if (Array.isArray(array)) {
+    return array.slice(1);
   }
+  return;
 };
 
-const tail = function(array) {
-  let tail = [];
-  array.splice(0, 1);
-  for (let x = 0; x < array.length; x++) {
-    tail.push(array[x]);
-  }
-  return tail;
-};
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-
 assertEqual(head([1, 2, 3]), 1);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

a13ce16cebb3d69c69375ebbdf22c1022739dd6a
Noah McMillan <noahm27@gmail.com>
2023-07-31 14:33:51 -0700

    inital commit of test files

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   return array[0];
 };
 
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,3 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual(2, 2);
